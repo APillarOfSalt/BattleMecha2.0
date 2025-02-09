8536ed5a3c471d3b2ba59e025dddf23540358e23
APillarOfSalt <landon.earl@hotmail.com>
2025-02-09 11:59:05 -0800

    Animations 2/3

--- defense_animation_controller.gd
+++ defense_animation_controller.gd
@@ -1,24 +1,35 @@
 extends Node2D
 class_name Defense_Animation_Controller
 
-var unit_atlas : Vector2i:
-	set(vec):
-		$mask.frame_coords = vec
+@onready var unit : Unit_Node
 
- #-1:shield_break, 0:hit, +1:shield_hit
-const ANIMS : Array[String] = ["shield_break","hit","shield_hit","death"]
-var anim : int = 0
-func setup(type:Module_Data.DMG_TYPES, arg:int):
-	$shield.visible = arg != 0
-	anim = arg + 1
-	set_dmg(type)
+func _setup(atk:Offensive_Animation_Controller):
+	pass
 
-var dmg_type : Module_Data.DMG_TYPES = -2
-func set_dmg(type:Module_Data.DMG_TYPES=-1):
-	$melee_hit.visible = type == -1
-	$p_hit.visible = type == Module_Data.DMG_TYPES.percussive
-	$v_hit.visible = type == Module_Data.DMG_TYPES.voltaic
-	$mask.visible = type == Module_Data.DMG_TYPES.voltaic
-	$c_hit.visible = type == Module_Data.DMG_TYPES.concussive
-
-
+enum ANIM{none=-1,death=0,hit,shield_break,shield_hit}
+var active_anim : ANIM = ANIM.none
+var active_anim_name : String
+const ANIMS : Dictionary = {
+	"death" : {
+		"msec" : 2500,
+		"hit" : 1500,
+	},
+	"hit" : {
+		"msec" : 1250,
+		"p" : 60,
+		"c" : 210,
+		"v" : 150,
+	},
+	"shield_break" : {
+		"msec" : 1600,
+		"p" : 530,
+		"c" : 380,
+		"v" : 590,
+	},
+	"shield_hit" : {
+		"msec" : 1550,
+		"p" : 60,
+		"c" : 210,
+		"v" : 280,
+	},
+}
